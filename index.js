009e98ee25655146b85702c8dcade1b413f7094c
Helen Spence <horselover7485@icloud.com>
2018-07-10 18:17:57 +0000

    adding todos fucitonal

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     console.log("helloworld");
+    
     $("#todobutton").click(function(){
         console.log("Button works")
+        var numberofpomadaros=$("#pomadarosinput").val()
         var nameoftodo=$("#todoinpput").val()
         console.log(nameoftodo)
-        $("ul").append("<li><input type='checkbox'/>"+nameoftodo+"<li>")
-        
-    
+        $("ul").append("<li><input type='checkbox' />" + nameoftodo + "<span class='time'>" + numberofpomadaros + " pomadaros </span></li>")
     })
-        
 })
+
